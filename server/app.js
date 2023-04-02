8a36d1b5c3fe76e72b14595c5c7a9d4cbd925e01
TJ Larson <90734478+tjl7357@users.noreply.github.com>
2023-04-02 16:04:12 -0400

    Added Redis to the Web App

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 // Lirbary Requires
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -8,6 +9,8 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const redis = require('redis');
 
 // Our Script Requires
 const router = require('./router.js');
@@ -24,33 +27,48 @@ mongoose.connect(dbURI).catch((err) => {
   }
 });
 
-// Sets up the Express Part of Our App
-const app = express();
-
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Creates the session settings
-app.use(session({
-  key: 'sessionID',
-  secret: 'Domo Arigato',
-  resave: false,
-  saveUninitialized: false,
-}));
-
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
-
-// Connects the app to the router
-router(app);
-
-// Starts the app
-app.listen(port, (err) => {
-  if (err) { throw err; }
-  console.log(`Listening on port ${port}`);
+// Set up the Redis Client
+const redisClient = redis.createClient({
+  url: process.env.REDISCLOUD_URL,
 });
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
+
+// Connect to the Redis Client
+redisClient.connect().then(() => {
+  // Sets up the Express Part of Our App
+  const app = express();
+
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  // Creates the session settings
+  app.use(session({
+    key: 'sessionID',
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: 'Domo Arigato',
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
+
+  // Connects the app to the router
+  router(app);
+
+  // Starts the app
+  app.listen(port, (err) => {
+    if (err) { throw err; }
+    console.log(`Listening on port ${port}`);
+  });
+});
+
+
