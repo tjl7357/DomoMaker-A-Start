e999949d25fb49afbc98dae7e0fd820909fbf038
TJ Larson <90734478+tjl7357@users.noreply.github.com>
2023-04-02 16:26:33 -0400

    Finished Domo C

--- server/router.js
+++ server/router.js
@@ -1,25 +1,26 @@
 // Requires
 const controllers = require('./controllers');
+const mid = require('./middleware');
 
 // Create the Router Function
 const router = (app) => {
   // Login Routes
-  app.get('/login', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
   // Signup Routes
-  app.get('/signup', controllers.Account.signupPage);
-  app.post('/signup', controllers.Account.signup);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
   // Logout Routes
-  app.get('/logout', controllers.Account.logout);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
   // Maker Routes
-  app.get('/maker', controllers.Domo.makerPage);
-  app.post('/maker', controllers.Domo.makeDomo);
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
   // Default Route
-  app.get('/', controllers.Account.loginPage);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 // exports
